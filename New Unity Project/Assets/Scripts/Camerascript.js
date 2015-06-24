b2b565abaa5c48d91e11873e46dc8047a7a8ec7f
albinolsen <o_j@hotmail.se>
2015-06-24 15:48:14 +0200

    Added playercontrolls v0.001

---
+++ New Unity Project/Assets/Scripts/Camerascript.js
@@ -0,0 +1,19 @@
+﻿var cam_speed = 1.00;
+var gui_size = 1;
+
+function Update () {
+var recdown = Rect(0, 0, Screen.width, gui_size);
+var recup = Rect(0, Screen.height - gui_size, Screen.width, gui_size);
+var recleft = Rect(0, 0, gui_size, Screen.height);
+var recright = Rect(Screen.width - gui_size, 0, gui_size, Screen.height);
+
+	if (recdown.Contains(Input.mousePosition))
+		transform.Translate(0, 0, -cam_speed, Space.world);
+	if (recup.Contains(Input.mousePosition))
+		transform.Translate(0, 0, cam_speed, Space.world);
+	if (recleft.Contains(Input.mousePosition))
+		transform.Translate(-cam_speed, 0, 0, Space.world);
+	if (recright.Contains(Input.mousePosition))
+		transform.Translate(cam_speed, 0, 0, Space.world);
+
+}
---
+++ New Unity Project/Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent (typeof (CharacterController))] //Adds a characther controller to player.
+public class Playercontroller : MonoBehaviour {
+	//handling
+	public float rotationSpeed = 30;
+	public float speed = 100;
+	public float boostSpeed = 5;
+	// System
+	private Quaternion targetRotation;
+	// Components
+	private CharacterController controller;
+	private Camera Cam;
+
+
+	void Start () {
+		controller = GetComponent<CharacterController>();
+		Cam = Camera.main;
+	
+	}
+
+	void Update () {
+		ControlMouse();
+	}
+
+	void ControlMouse() {
+		Vector3 mousePos = Input.mousePosition;
+		mousePos = Cam.ScreenToWorldPoint(new Vector3(mousePos.x,mousePos.y,Cam.transform.position.y - transform.position.y));
+		targetRotation = Quaternion.LookRotation (mousePos - new Vector3(transform.position.x,0,transform.position.z));
+		transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle (transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
+
+		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));		
+		Vector3 motion = input;
+		
+		controller.Move (motion * Time.deltaTime);	
+	}
+}
