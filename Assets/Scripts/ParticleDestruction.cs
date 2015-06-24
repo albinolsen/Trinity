37a103d033a440858864e8b93a87fbf1676d1d59
itgsod-lukas-vikstrom <lukasvikstrom@gmail.com>
2015-06-25 01:39:19 +0200

    Fixed minor UI bug + Added particle death timer

---
+++ Assets/Scripts/ParticleDestruction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleDestruction : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+
+			Destroy (gameObject,5f);
+
+	}
+	
+	// Update is called once per frame
+
+}
