0cdd1e03bcc5048018ef4b25b2f7f304cca8b628
itgsod-lukas-vikstrom <lukasvikstrom@gmail.com>
2015-06-25 18:28:13 +0200

    Fixed gun angles for the reworked bb1

--- Assets/Scripts/GunAimingAngle.js
+++ Assets/Scripts/GunAimingAngle.js
@@ -5,12 +5,12 @@ function Update () {
 	
 if(Input.GetKey("w")) {
 	if(angle < maxAngle){
-		transform.Rotate(0,0,-0.1);
+		transform.Rotate(0.1,0,0);
 		angle += 0.1;
  }}
 if(Input.GetKey("s")){
 	if(angle > 0.1){
-		transform.Rotate(0,0,0.1);
+		transform.Rotate(-0.1,0,0);
 		angle -= 0.1;
 	}
 
--- Assets/Scripts/GunAimingBack.js
+++ Assets/Scripts/GunAimingBack.js
@@ -1,16 +1,15 @@
-﻿#pragma strict
-static var angle : float = 0;
+﻿static var angle : float = 0;
 var maxAngle = 30;
 function Update () {
 	
 if(Input.GetKey("w")) {
 	if(angle < maxAngle){
-		transform.Rotate(0,0,-0.1);
+		transform.Rotate(0.1,0,0);
 		angle += 0.1;
  }}
 if(Input.GetKey("s")){
 	if(angle > 0.1){
-		transform.Rotate(0,0,0.1);
+		transform.Rotate(-0.1,0,0);
 		angle -= 0.1;
 	}
 
